0514d30740a99d6ab7082e79f0d73c22258f9d84
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2018-12-22 13:36:39 -0500

    Search OpenWeatherAPI with the coordinates pulled from Google's Geolocation API

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,43 +1,27 @@
 $(document).ready(function() {
     
+var latitude;
+var longitude;
+
     function displayWeather() {
         var APIKey = "dd90b036fea16c286d0b1e900c84157c";
         var zip = "19146"
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" +
-        zip + "&units=imperial&appid=" + APIKey
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + APIKey
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(queryURL);
             console.log(response);
           //Create weather icon
             var weatherIconURL = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
             var weatherIcon = $("<img>");
             weatherIcon.attr("src", weatherIconURL);
-            console.log(weatherIconURL);
             $("#weather").append(weatherIcon);
 
             $("#weather").append(JSON.stringify(response));
         });
     };
-          
-    displayWeather();
-
-    function getLocation(){
-        var APIKey = "7973f370d72acc8a88dbd18f23806729a26e319cb848dac934bb95b0";
-        var queryURL = "https://api.ipdata.co?api-key=" + APIKey;
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(queryURL);
-            console.log(response);
-        });
-    };
-    getLocation();
 
     function googleLocation() {
         var APIKey = "AIzaSyCh4H_JTQPRDjRLXjGkPYaYmfdaX6y5ncI";
@@ -48,9 +32,9 @@ $(document).ready(function() {
             method: "POST"
         }).then(function(response) {
             console.log(response);
-            console.log(queryURL);
-            var latitude = response.location.lat;
-            var longitude = response.location.lng;
+            latitude = response.location.lat;
+            longitude = response.location.lng;
+            displayWeather();
         });
     };
     // Commented this out to prevent overusing the API key for now
