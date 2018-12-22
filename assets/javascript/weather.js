cb3f0569dc351b7984ab59dff1c3b79c26cd3680
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2018-12-22 11:34:41 -0500

    Create an icon based on the weather

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,13 +1,17 @@
-var APIKey = "dd90b036fea16c286d0b1e900c84157c";
-var zip = "19146"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" +
-zip + "&units=imperial&appid=" + APIKey
+$(document).ready(function() {
+    var APIKey = "dd90b036fea16c286d0b1e900c84157c";
+    var zip = "19146"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" +
+    zip + "&units=imperial&appid=" + APIKey
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-    console.log("hello");
-})
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+        var weatherIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+        console.log(weatherIcon);
+        $("#weather").text(JSON.stringify(response));
+    })
+});
