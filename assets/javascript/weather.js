aba58e5269e12a41ab424a0b6377b28649c5e3f1
sdreyes <sheshechan90@gmail.com>
2019-01-01 19:49:57 -0500

    Find the high and low temperatures for the next 5 days

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -13,15 +13,17 @@ var longitude;
         }).then(function(response) {
             console.log(response);
             console.log(queryURL);
+            // Show the user the city we are displaying weather for
             var locationHeader = $("<small>");
             locationHeader.addClass("text-muted");
             locationHeader.text(" in " + response.name);
             $("#weather-location").append(locationHeader);
+            // Note the last time the weather was updated - should be roughly every 20 minutes
             var lastUpdateDisplay = $("<span>");
             lastUpdateDisplay.addClass("text-muted");
             lastUpdateDisplay.text(" Last Update: " + moment(response.dt, "X").format("dddd, MMMM Do YYYY, h:mm a"));
             $("#weather-header").append(lastUpdateDisplay);
-            //Create and display weather icon
+            //Create weather icon
             var weatherIconURL = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
             var weatherIcon = $("<img>");
             weatherIcon.attr("src", weatherIconURL);
@@ -31,7 +33,44 @@ var longitude;
             $("#weather").append("<h4 class='d-inline'> " + response.weather[0].main + "</h4>");
             $("#weather").append("<h5>Humidity: " + response.main.humidity + "%</h5>");
             $("#weather").append("<h5>Wind speed: " + response.wind.speed + " mph</h5>");
+
+            displayForecast();
             
+        });
+    };
+
+    function displayForecast() {
+        var APIKey = "dd90b036fea16c286d0b1e900c84157c";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + APIKey
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+
+            var forecasts = response.list;
+            console.log(forecasts);
+            var temps = [];
+            var weatherCodes = [];
+
+            $.each(forecasts, function(i, forecast) {
+                if (temps.length != 8) {
+                    temps.push(forecast.main.temp);
+                    weatherCodes.push(forecast.weather[0].icon);
+                }
+                else {
+                    var forecastDisplay = $("<div>");
+                    forecastDisplay.addClass("row");
+                    var forecastDayDisplay = $("<div>");
+                    forecastDayDisplay.addClass("column")
+                    forecastDayDisplay.append("<h6>" + moment(forecast.dt, "X").format("ddd, MMM Do") + "</h6>");
+                    var highTemp = Math.max.apply(null, temps);
+                    var lowTemp = Math.min.apply(null, temps);
+                    
+                    temps = [];
+                };
+            });
         });
     };
 
