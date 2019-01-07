2679a2df8615087983240aaf27c718848bfc5628
rfloeter <rainer.r.floeter@gmail.com>
2019-01-06 23:34:19 -0500

    update to todolist

--- assets/javascript/todolist.js
+++ assets/javascript/todolist.js
@@ -1,57 +1,74 @@
-    //Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDzBCE_o3i2jGTkD2BWuiZmppUYbEyP5pI",
-        authDomain: "project1-18921.firebaseapp.com",
-        databaseURL: "https://project1-18921.firebaseio.com",
-        projectId: "project1-18921",
-        storageBucket: "project1-18921.appspot.com",
-        messagingSenderId: "55286196596"
-      };
-      firebase.initializeApp(config);
-      
-      var database = firebase.database();
-  
-   //create the initial todocount variable
-   var toDoCount = 0;
-   
-  window.onload = function() {
-      //user clicked on the add button in the to-do field add that text into the to-do text
-      $('#add-to-do').on('click', function(event) {
-          event.preventDefault();
-          //assign variable to the value entered into the textbox
-          var value = document.getElementById('to-do').value; 
-          //test value
-          console.log(value);
-          //prepend values into the html and add checkmark, checkbox, and line break to make list
-          var linebreak = "<br/>";
-          var todoclose = $("<button>");
-          todoclose.attr("data-to-do", toDoCount);
-          todoclose.addClass("checkbox");
-          todoclose.text("☑");
-  
-          //prepend values to html
-          $("#to-dos").prepend(linebreak);
-          $("#to-dos").prepend(value);
-          $("#to-dos").prepend(todoclose);
-          toDoCount++;
-  
-  
-  //to remove item from checklist
-          var i;
-          for(i = 0; i < todoclose.length; i++) {
-              todoclose[i].onclick = function() {
-                  var removal = todoclose[i];
-                  $("#to-dos").remove();
-              }
-          }
-      });
-   
-      }
-  
-  
-      
-  
-      
-  
-      
-      
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDZwIxXFNk928XzjMRZ6bpXVkeuEMbwIUI",
+    authDomain: "in-the-know-a0b16.firebaseapp.com",
+    databaseURL: "https://in-the-know-a0b16.firebaseio.com",
+    projectId: "in-the-know-a0b16",
+    storageBucket: "",
+    messagingSenderId: "447330895711"
+  };
+  firebase.initializeApp(config);
+    
+var database = firebase.database();
+
+//create the initial todocount variable
+var toDoCount = 0;
+
+//initialize todos for firebase
+var todos;
+
+$(function() {
+  //user clicked on the add button in the to-do field add that text into the to-do text
+  $('#add-to-do').on('click', function(event) {
+    event.preventDefault();
+
+    //assign variable to the value entered into the textbox
+    var value = document.getElementById('to-do').value;
+    //test value
+    console.log(value);
+
+    var todoitem = $("#to-dos");
+    todoitem.attr("item-");
+    //prepend values into the html and add checkmark, checkbox, and line break to make list
+    var linebreak = "<br/>";
+    var todoclose = $("<button>");
+    todoclose.attr("data-to-do", toDoCount);
+    todoclose.addClass("checkbox");
+    todoclose.text("☑");
+
+    //prepend values to html
+    $("<div/>", {
+        "class": "to-do-item"
+      })
+      .append([todoclose, value, linebreak])
+      .appendTo($("#to-dos"));
+    toDoCount++;
+
+    //to remove item from checklist
+    $(document.body).on("click", ".to-do-item", function() {
+      $(this).remove();
+    });
+  });
+
+  //Firebase Stuff
+  $("#add-to-do").on('click', function(event){
+    event.preventDefault();
+    todos = $(".to-do-item").val().trim();
+
+    console.log(todos);
+ 
+    database.ref().push({
+    todos: todos
+    });
+    });
+    //firebase watcher
+    database.ref().limitToLast(1).on('value', snapshot => {
+    snapshot.forEach(snap => {
+        todos = snap.child("List").val();
+    });
+    });
+
+
+});
+
+
