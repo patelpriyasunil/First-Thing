eae6dc444b011dc5b1e3f9d80b06bcb6d4afcb0c
sdreyes <sheshechan90@gmail.com>
2019-02-18 21:18:03 -0500

    Replace NYT API with News API

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -1,62 +1,63 @@
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-   'api-key': "e6e3a8e67e8a485dbec602712a2de5fe"
-});
-
+var url = "https://newsapi.org/v2/top-headlines?sources=the-wall-street-journal&apiKey=124b8f94b39c40ddb343032372b6e31a";
+var req = new Request(url);
 $.ajax({
    url: url,
-   method: 'GET',
-}).done(function (response) {
-   console.log(response);
+   method: "GET",
+ }) .then(function(response) {
+            return response
+      }).then(data=>{
+         
+         console.log(data);
+         var date = data.articles[0].content
+         console.log(date);
+      
 
-   var articles = 0;
+   for (var i = 0; i <= 2; i++) {
 
-   $.each(response.results, function (i, article) {
-      if (article.multimedia.length !== 0 && articles !== 3) {
-         console.log(article.multimedia.length);
-         // Creating and storing a div tag
-         var articleDiv = $("<div>");
-         //  adding border and margin
-         articleDiv.addClass("col text-left")
-         articleDiv.addClass("col text-left m-1")
       
-         // Storing the imgURL
-         var imgURL = article.multimedia[0].url;
-      
-         // Creating an element to hold the image
-         var image = $("<img>").attr("src", imgURL);
-         // adding margin around the image
-         image.addClass("m-1")
-      
-         // Appending the image
-         articleDiv.append(image);
-      
-         // Storing the title data
-         var title = article.title;
-      
-         // Displaying the title and linking it to the article url
-         articleDiv.append("<h6 class='d-inline'>" + "<a href='" + article.url + "' target='_blank'>" + title + "</a>" + "</h6>");
-      
-         // Storing the date data
-         var date = article.created_date;
-      
-         // Displaying the date
-         articleDiv.append("<h6>" + moment(date).format("dddd, MMMM Do YYYY, h:mm a") + "</h6>");
-      
-         // Storing the abstract data
-         var abstract = article.abstract;
-      
-         // Creating an element to hold the abstract
+// //       // Creating and storing a div tag
+      var articleDiv = $("<div>");
+// //       //  adding border and margin
+      articleDiv.addClass("col text-left border border-info m-1")
+
+
+// //          // // Storing the imgURL
+         var img = `<img style="height:80px; width:80px" class ="m-1 img-thumbnail"  src= ${data.articles[i].urlToImage}>`
+
+// //          // // Creating an element to hold the image
+        
+// //          // // Appending the image
+         articleDiv.append(img);
+         
+
+// //          // Storing the title data
+         var title = data.articles[i].title;
+
+// //          // Displaying the title and linking it to the article url
+         articleDiv.append("<h6 class='d-inline'>" + "<a href='"+ data.articles[i].url + "' target='_blank'>" + title + "</a>" + "</h6>");
+        
+
+
+// //          // Storing the date data
+         var date = data.articles[i].publishedAt;
+
+//          // Displaying the date
+         articleDiv.append("<h6>" + moment(date).format("dddd, MMMM Do YYYY, h:mm a" ) + "</h6>");
+
+        
+//          // Storing the abstract data
+         var abstract = data.articles[i].content;
+        
+//          // Creating an element to hold the abstract
          var pThree = $("<p>").text(abstract);
-      
-         // Displaying the abstract
-         articleDiv.append(pThree);
-         articleDiv.append("<hr>");
-      
-         // Appending the articleDiv to the HTML page in the "#acticle-section" div
-         $("#article-section").append(articleDiv);
 
-         articles++;
-      }
-   })
+//          // Displaying the abstract
+         articleDiv.append(pThree);
+
+
+
+//    // Appending the articleDiv to the HTML page in the "#acticle-section" div
+   $("#article-section").append(articleDiv);
+
+   }   
 });
